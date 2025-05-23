426c79bfb2793c4b9fee7eb5400fcc7e7193a10d
seungfwani <seungfwani2@gmail.com>
2025-05-23 15:13:34 +0900

    [THIRDACT-69] fix: websocket_router.py

--- server/api/routers/v2/websocket_router.py
+++ server/api/routers/v2/websocket_router.py
@@ -2,8 +2,9 @@ import asyncio
 import json
 import logging
 
-from fastapi import APIRouter, WebSocket, Query, WebSocketDisconnect
+from fastapi import APIRouter, WebSocket, Depends, Query, WebSocketDisconnect
 from fastapi.encoders import jsonable_encoder
+from sqlalchemy.orm import Session
 
 from api.models.api_model import api_response_wrapper
 from api.models.dag_model import AirflowDagRunModel, TaskInstanceResponse
@@ -23,7 +24,8 @@ router = APIRouter(
 @router.websocket("/dag-runs-history")
 async def websocket_dag_history(websocket: WebSocket,
                                 dag_id: str = Query(...),
-                                user_id: str = Query("anonymous")):
+                                user_id: str = Query("anonymous"),
+                                db: Session = Depends(get_db)):
     await websocket.accept()
     client_ip = websocket.client.host
     logger.info(f"🔌 WebSocket connected from IP: {client_ip}, dag_id: {dag_id}, user_id: {user_id}")
@@ -52,37 +54,36 @@ async def websocket_dag_history(websocket: WebSocket,
             # run_id 가 설정된 경우 주기적으로 task 정보도 전송
             if current_run_id:
                 try:
-                    with next(get_db()) as db:
-                        with next(get_airflow_client()) as airflow_client:
-                            airflow_dag_run_history, tasks = get_all_tasks_by_run_id(current_run_id, airflow_client, db)
-                            tasks_data = TaskInstanceResponse.from_data(airflow_dag_run_history, tasks)
-                            if tasks_data != old_tasks_data:
-                                logger.info(f"🙆 Have a different tasks of run_id: {current_run_id}")
+                    with next(get_airflow_client()) as airflow_client:
+                        airflow_dag_run_history, tasks = get_all_tasks_by_run_id(current_run_id, airflow_client, db)
+                        tasks_data = TaskInstanceResponse.from_data(airflow_dag_run_history, tasks)
+                        if tasks_data != old_tasks_data:
+                            logger.info(f"🙆 Have a different tasks of run_id: {current_run_id}")
 
-                                async def get_tasks():
-                                    return tasks_data
+                            async def get_tasks():
+                                return tasks_data
 
-                                task_response = (await api_response_wrapper(get_tasks)()).model_dump()
-                                task_response["type"] = "tasks"
-                                await websocket.send_json(jsonable_encoder(task_response))
-                                old_tasks_data = tasks_data
-                            else:
-                                logger.info(f"🤷 Nothing to different tasks of run_id: {current_run_id}")
+                            task_response = (await api_response_wrapper(get_tasks)()).model_dump()
+                            task_response["type"] = "tasks"
+                            await websocket.send_json(jsonable_encoder(task_response))
+                            old_tasks_data = tasks_data
+                        else:
+                            logger.info(f"🤷 Nothing to different tasks of run_id: {current_run_id}")
                 except Exception as e:
                     logger.warning(f"🚫 Failed to fetch task info: {e}")
 
             # DAG 실행 이력 조회
             logger.info(f"🔄 Check dag runs of dag_id: {dag_id}")
-            with next(get_db()) as db:
-                dag_runs = get_all_dag_runs_of_all_versions(dag_id, db)
+            dag_runs = get_all_dag_runs_of_all_versions(dag_id, db)
             if old_dag_runs == dag_runs:
                 logger.info(f"🤷 Nothing to different dag runs of dag_id: {dag_id}")
-                await asyncio.sleep(10)
+                await asyncio.sleep(3)
                 continue
             logger.info(f"🙆 Have a different dag runs of dag_id: {dag_id}")
 
+            response_data = [AirflowDagRunModel.from_orm(data) for data in dag_runs]
             async def get_dag_runs():
-                return [AirflowDagRunModel.from_orm(data) for data in dag_runs]
+                return response_data
 
             # 클라이언트에게 전송
             response = (await api_response_wrapper(get_dag_runs)()).model_dump()
