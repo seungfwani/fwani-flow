4da8adc4fe3749526cca8d8ef278a286c75e2fd3
seungfwani <seungfwani2@gmail.com>
2025-08-11 12:04:27 +0900

    refactor: delete unused code

--- fwani_airflow_plugin/__init__.py
+++
@@ -1,9 +0,0 @@
-from airflow.plugins_manager import AirflowPlugin
-
-from fwani_airflow_plugin.routes import fwani_api_bp
-from fwani_airflow_plugin.swagger import swagger_bp
-
-
-class FwaniAirflowPlugin(AirflowPlugin):
-    name = "fwani_airflow_plugin"
-    flask_blueprints = [fwani_api_bp, swagger_bp]
