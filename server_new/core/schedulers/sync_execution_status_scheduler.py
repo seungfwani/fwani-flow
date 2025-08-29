3dc86522dd143e3bbcc38d6ff9b513467dae2e11
seungfwani <seungfwani2@gmail.com>
2025-08-29 17:06:31 +0900

    feat: update sync execution

--- server_new/core/schedulers/sync_execution_status_scheduler.py
+++ server_new/core/schedulers/sync_execution_status_scheduler.py
@@ -3,6 +3,7 @@ import logging
 from airflow.models import DagRun as AirflowDagRun
 from sqlalchemy import and_
 
+from core.airflow_client import get_airflow_client, get_airflow_client_context
 from core.database import SessionLocalBaseDB, SessionLocalAirflowDB
 from models.db.flow_execution_queue import FlowExecutionQueue
 from models.domain.enums import FlowExecutionStatus
@@ -24,14 +25,21 @@ def sync_execution_status_job():
             ))
             .all())
         logger.info(f"non terminated executions: {len(non_terminated_execution)}")
-        for execution in non_terminated_execution:
-            # 아직 대기중/진행중 이므로, 상태 체크 후 반환
-            airflow_dag_run = (airflow_db.query(AirflowDagRun)
-                               .filter(and_(AirflowDagRun.dag_id == execution.dag_id,
-                                            AirflowDagRun.run_id == execution.run_id))
-                               .first())
-            if airflow_dag_run:
-                execution.status = FlowExecutionStatus.from_str(airflow_dag_run.state).value
+        with get_airflow_client_context() as airflow_client:
+            for execution in non_terminated_execution:
+                if execution.flow_snapshot_id is None:
+                    logger.warning(f"⚠️ kill execution({execution.id}) cause snapshot is deleted.")
+                    airflow_client.kill(execution.dag_id, execution.run_id)
+                    execution.status = FlowExecutionStatus.KILLED.value
+                else:
+                    # 아직 대기중/진행중 이므로, 상태 체크 후 반환
+                    logger.info(f"🔄️ check status execution({execution.id})")
+                    airflow_dag_run = (airflow_db.query(AirflowDagRun)
+                                       .filter(and_(AirflowDagRun.dag_id == execution.dag_id,
+                                                    AirflowDagRun.run_id == execution.run_id))
+                                       .first())
+                    if airflow_dag_run:
+                        execution.status = FlowExecutionStatus.from_str(airflow_dag_run.state).value
 
         meta_db.commit()
         logger.info("✅ Finished synchronizing execution status.")
